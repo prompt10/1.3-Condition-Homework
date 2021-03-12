c3443251bb4fd7b523a31b0e22739b2b24fa03ad
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-03-12 17:45:14 +0700

    ++Adding all the variable I think I need --And yes, was trying Windows Form for a WEEK. Ain't getting no progress from that.

---
+++ Condition Homework/Condition Homework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Condition_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1, p2, t1, t2, x, y;
+            string mode, Input1, Input2;
+            Console.Write("Please input the mode: ");
+            mode = Console.ReadLine();
+        }
+    }
+}
