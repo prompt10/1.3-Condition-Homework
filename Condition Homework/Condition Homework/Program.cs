c73d2c7fdcb2e039d258b68119bc048ba63b38d4
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-03-12 18:36:57 +0700

    ++Add time mode successfully, My brain went short circuit and overheat to find which position in the god-dang solution is the right one.

--- Condition Homework/Condition Homework/Program.cs
+++ Condition Homework/Condition Homework/Program.cs
@@ -10,6 +10,35 @@ namespace Condition_Homework
             string mode, Input1, Input2;
             Console.Write("Please input the mode: ");
             mode = Console.ReadLine();
+            Console.WriteLine("Please enter your mouse position ");
+            
+            Console.Write("time: ");
+            Input1 = Console.ReadLine();
+            x = double.Parse(Input1);
+            Console.Write("price: ");            
+            Input2 = Console.ReadLine();
+            y = double.Parse(Input2);
+
+            if (x < 0)
+            {
+                Console.WriteLine("Invalid mouse position");
+            }
+            else
+            {
+                if (mode == "time")
+                {
+                    p1 = Math.Pow(x - 1, 2);
+                    Console.WriteLine("({0},{1})", x, p1);
+                }
+                else if (mode == "price")
+                {
+                    t2 = x;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Mode");
+                }
+            }
         }
     }
 }
